e22cd13f93f00a99d542c74e774a89fa0262a078
Hao ZHANG <caesarhao@126.com>
2016-03-31 10:25:37 +0200

    Update BezierCurveTest.java

--- src/funtest/BezierCurveTest.java
+++ src/funtest/BezierCurveTest.java
@@ -10,6 +10,7 @@ public class BezierCurveTest {
 		double[] ctlPXs = {0, 2, 2};
 		double[] ctlPYs = {2, 2, 0};
 		bCurve = new BezierCurve(ctlPXs, ctlPYs);
+		bCurve.setBDebugMode(true);
 	}
 	public static void main(String[] args) {
 		BezierCurveTest test = new BezierCurveTest();
@@ -18,7 +19,7 @@ public class BezierCurveTest {
 			Point p = null;
 			for (double i = 0; i < 2.1; i += 0.1){
 				p = test.bCurve.calcWithX(i);
-				System.out.println(p + "\t t: " + p.t);
+				System.out.println(p);
 			}
 		} catch (BezierCurveException e) {
 			e.printStackTrace();
